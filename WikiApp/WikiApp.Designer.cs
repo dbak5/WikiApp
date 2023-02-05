9be7c28702a6e21eca72c70d26d3861609ac7816
DaHye Baker <dahye.baker@jawin.com.au>
2023-02-05 10:16:12 +0800

    Add project files.

---
+++ WikiApp/WikiApp.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace WikiApp
+{
+    partial class WikiApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextboxSearch = new System.Windows.Forms.TextBox();
+            this.StatusLabel = new System.Windows.Forms.StatusStrip();
+            this.ListViewDataStructure = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // TextboxSearch
+            // 
+            this.TextboxSearch.Location = new System.Drawing.Point(137, 173);
+            this.TextboxSearch.Name = "TextboxSearch";
+            this.TextboxSearch.Size = new System.Drawing.Size(100, 20);
+            this.TextboxSearch.TabIndex = 0;
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.Location = new System.Drawing.Point(0, 428);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(800, 22);
+            this.StatusLabel.TabIndex = 1;
+            this.StatusLabel.Text = "Status";
+            // 
+            // ListViewDataStructure
+            // 
+            this.ListViewDataStructure.HideSelection = false;
+            this.ListViewDataStructure.Location = new System.Drawing.Point(355, 156);
+            this.ListViewDataStructure.Name = "ListViewDataStructure";
+            this.ListViewDataStructure.Size = new System.Drawing.Size(121, 97);
+            this.ListViewDataStructure.TabIndex = 2;
+            this.ListViewDataStructure.UseCompatibleStateImageBehavior = false;
+            // 
+            // WikiApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ListViewDataStructure);
+            this.Controls.Add(this.StatusLabel);
+            this.Controls.Add(this.TextboxSearch);
+            this.Name = "WikiApp";
+            this.Text = "Data Structures Wiki";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextboxSearch;
+        private System.Windows.Forms.StatusStrip StatusLabel;
+        private System.Windows.Forms.ListView ListViewDataStructure;
+    }
+}
+
---
+++ WikiApp/WikiApp.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Author: DaHye Baker
+// Student ID: 30063368
+// Organisation: South Metropolitan TAFE
+// Description: Wiki Application on Data Structures
+// A program that presents information and definition on data structures
+
+namespace WikiApp
+{
+    public partial class WikiApp : Form
+    {
+        public WikiApp()
+        {
+            InitializeComponent();
+        }
+
+        #region Variables
+        // 9.1 Create a global 2D string array, use static variables for the dimensions (row = 4, column = 12),
+        private const int Row = 4;
+        private const int Col = 12;
+        string[,] WikiArray = new string[Row, Col];
+
+        private const string StatusStripItemFound = "Item found";
+        private const string StatusStripItemNotFound = "Item not found";
+        private const string StatusStripErrorInteger = "Error! Please enter a valid integer";
+        private const string StatusStripErrorNoValue = "Error! Please enter a value";
+        private const string StatusStripErrorSelectItem = "Error! Please select an item from the list";
+        private const string StatusStripUpdateSuccess = "Data successfully updated";
+        #endregion
+
+        #region Events & Buttons
+        // 9.2 Create an ADD button that will store the information from the 4 text boxes into the 2D array
+        private void ButtonAdd_MouseClick(object sender, MouseEventArgs e)
+        {
+            // ADD CODE
+        }
+
+        // 9.3 Create and EDIT button that will allow the user to modify any information from the 4 text boxes into the 2D array
+        private void ButtonEdit_MouseClick(object sender, MouseEventArgs e)
+        {
+            // ADD CODE
+        }
+
+        // 9.4 Create a DELETE button that removes all the information from a single entry of the array; the user must be prompted before the final deletion occurs
+        private void ButtonDelete_MouseClick(object sender, MouseEventArgs e)
+        {
+            // ADD CODE
+        }
+
+        // 9.5 Create a CLEAR method to clear the four text boxes so a new definition can be added
+        private void ButtonClear_MouseClick(object sender, MouseEventArgs e)
+        {
+            // ADD CODE
+        }
+
+        // 9.10 Create a SAVE button so the information from the 2D array can be written into a binary file called definitions.dat which is sorted by Name, ensure the user has the option to select an alternative file. Use a file stream and BinaryWriter to create the file
+        private void ButtonSave_MouseClick(object sender, MouseEventArgs e)
+        {
+            // ADD CODE
+        }
+
+        // 9.11 Create a LOAD button that will read the information from a binary file called definitions.dat into the 2D array, ensure the user has the option to select an alternative file. Use a file stream and BinaryReader to complete this task
+        private void ButtonLoad_MouseClick(object sender, MouseEventArgs e)
+        {
+            // ADD CODE
+        }
+
+        private void ButtonBinarySearch_MouseClick(object sender, MouseEventArgs e)
+        {
+            BinarySearch();
+            // ADD CODE
+        }
+        
+        private void ButtonSort_MouseClick(object sender, MouseEventArgs e)
+        {
+            BubbleSortFunction();
+            // ADD CODE
+        }
+        #endregion
+
+        #region Methods & Functions
+        // 9.6 Write the code for a Bubble Sort method to sort the 2D array by Name ascending, ensure you use a separate swap method that passes the array element to be swapped (do not use any built-in array methods)
+        /// <summary>
+        /// Bubble sort function
+        /// </summary>
+        private void BubbleSortFunction()
+        {
+            // ADD CODE
+        }
+
+        // 9.7 Write the code for a Binary Search for the Name in the 2D array and display the information in the other textboxes when found, add suitable feedback if the search in not successful and clear the search textbox (do not use any built-in array methods)
+        /// <summary>
+        /// Binary search function that would return a index
+        /// </summary>
+        /// <returns></returns>
+        private int BinarySearch()
+        {
+            // ADD CODE
+            return -1;
+        }
+
+        // 9.8 Create a display method that will show the following information in a ListView: Name and Category
+        /// <summary>
+        /// Display function
+        /// </summary>
+        private void DisplayFunction()
+        {
+            // ADD CODE
+        }
+
+        // 9.9 Create a method so the user can select a definition (Name) from the ListView and all the information is displayed in the appropriate Textboxes
+        /// <summary>
+        /// Select function
+        /// </summary>
+        private void SelectFunction()
+        {
+            // ADD CODE
+        }
+
+        /// <summary>
+        /// Update list box function
+        /// </summary>
+        private void UpdateListDataStructure()
+        {
+            // ADD CODE
+        }
+        #endregion
+    }
+}
