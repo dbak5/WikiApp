7038397d54cd05d7a4059eec4077f1c25dffe50b
30063368 <30063368@tafe.wa.edu.au>
2023-02-17 11:20:16 +0800

    started adding buttons and methods

--- WikiApp/WikiApp.Designer.cs
+++ WikiApp/WikiApp.Designer.cs
@@ -28,14 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem("");
             this.TextboxSearch = new System.Windows.Forms.TextBox();
             this.StatusLabel = new System.Windows.Forms.StatusStrip();
             this.ListViewDataStructure = new System.Windows.Forms.ListView();
+            this.ButtonAdd = new System.Windows.Forms.Button();
+            this.ButtonSort = new System.Windows.Forms.Button();
+            this.ButtonLoad = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ButtonClear = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.StatusProgressBar = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
             // 
             // TextboxSearch
             // 
-            this.TextboxSearch.Location = new System.Drawing.Point(137, 173);
+            this.TextboxSearch.Location = new System.Drawing.Point(470, 39);
             this.TextboxSearch.Name = "TextboxSearch";
             this.TextboxSearch.Size = new System.Drawing.Size(100, 20);
             this.TextboxSearch.TabIndex = 0;
@@ -50,18 +64,89 @@
             // 
             // ListViewDataStructure
             // 
+            this.ListViewDataStructure.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
             this.ListViewDataStructure.HideSelection = false;
-            this.ListViewDataStructure.Location = new System.Drawing.Point(355, 156);
+            this.ListViewDataStructure.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3,
+            listViewItem4});
+            this.ListViewDataStructure.Location = new System.Drawing.Point(355, 103);
             this.ListViewDataStructure.Name = "ListViewDataStructure";
-            this.ListViewDataStructure.Size = new System.Drawing.Size(121, 97);
+            this.ListViewDataStructure.Size = new System.Drawing.Size(362, 226);
             this.ListViewDataStructure.TabIndex = 2;
             this.ListViewDataStructure.UseCompatibleStateImageBehavior = false;
+            this.ListViewDataStructure.View = System.Windows.Forms.View.Details;
+            // 
+            // ButtonAdd
+            // 
+            this.ButtonAdd.Location = new System.Drawing.Point(90, 39);
+            this.ButtonAdd.Name = "ButtonAdd";
+            this.ButtonAdd.Size = new System.Drawing.Size(75, 23);
+            this.ButtonAdd.TabIndex = 3;
+            this.ButtonAdd.Text = "Add New Record";
+            this.ButtonAdd.UseVisualStyleBackColor = true;
+            this.ButtonAdd.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ButtonAdd_MouseClick);
+            // 
+            // ButtonSort
+            // 
+            this.ButtonSort.Location = new System.Drawing.Point(89, 103);
+            this.ButtonSort.Name = "ButtonSort";
+            this.ButtonSort.Size = new System.Drawing.Size(75, 23);
+            this.ButtonSort.TabIndex = 4;
+            this.ButtonSort.Text = "Sort Data";
+            this.ButtonSort.UseVisualStyleBackColor = true;
+            this.ButtonSort.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ButtonSort_MouseClick);
+            // 
+            // ButtonLoad
+            // 
+            this.ButtonLoad.Location = new System.Drawing.Point(130, 155);
+            this.ButtonLoad.Name = "ButtonLoad";
+            this.ButtonLoad.Size = new System.Drawing.Size(75, 23);
+            this.ButtonLoad.TabIndex = 5;
+            this.ButtonLoad.Text = "Load Data";
+            this.ButtonLoad.UseVisualStyleBackColor = true;
+            this.ButtonLoad.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ButtonLoad_MouseClick);
+            // 
+            // ButtonClear
+            // 
+            this.ButtonClear.Location = new System.Drawing.Point(130, 236);
+            this.ButtonClear.Name = "ButtonClear";
+            this.ButtonClear.Size = new System.Drawing.Size(75, 23);
+            this.ButtonClear.TabIndex = 6;
+            this.ButtonClear.Text = "Clear Data";
+            this.ButtonClear.UseVisualStyleBackColor = true;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 7;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // StatusProgressBar
+            // 
+            this.StatusProgressBar.Location = new System.Drawing.Point(459, 426);
+            this.StatusProgressBar.Name = "StatusProgressBar";
+            this.StatusProgressBar.Size = new System.Drawing.Size(289, 23);
+            this.StatusProgressBar.TabIndex = 8;
             // 
             // WikiApp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StatusProgressBar);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.ButtonClear);
+            this.Controls.Add(this.ButtonLoad);
+            this.Controls.Add(this.ButtonSort);
+            this.Controls.Add(this.ButtonAdd);
             this.Controls.Add(this.ListViewDataStructure);
             this.Controls.Add(this.StatusLabel);
             this.Controls.Add(this.TextboxSearch);
@@ -77,6 +162,16 @@
         private System.Windows.Forms.TextBox TextboxSearch;
         private System.Windows.Forms.StatusStrip StatusLabel;
         private System.Windows.Forms.ListView ListViewDataStructure;
+        private System.Windows.Forms.Button ButtonAdd;
+        private System.Windows.Forms.Button ButtonSort;
+        private System.Windows.Forms.Button ButtonLoad;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button ButtonClear;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ProgressBar StatusProgressBar;
     }
 }
 
--- WikiApp/WikiApp.cs
+++ WikiApp/WikiApp.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 // Author: DaHye Baker
 // Student ID: 30063368
@@ -28,13 +30,9 @@ namespace WikiApp
         private const int Row = 12;
         private const int Col = 4;
         string[,] WikiArray = new string[Row, Col];
+        bool sorted = false;
+        bool filled = false;
 
-        private const string StatusStripItemFound = "Item found";
-        private const string StatusStripItemNotFound = "Item not found";
-        private const string StatusStripErrorInteger = "Error! Please enter a valid integer";
-        private const string StatusStripErrorNoValue = "Error! Please enter a value";
-        private const string StatusStripErrorSelectItem = "Error! Please select an item from the list";
-        private const string StatusStripUpdateSuccess = "Data successfully updated";
         #endregion
 
         #region Events & Buttons
@@ -71,7 +69,7 @@ namespace WikiApp
         // 9.11 Create a LOAD button that will read the information from a binary file called definitions.dat into the 2D array, ensure the user has the option to select an alternative file. Use a file stream and BinaryReader to complete this task
         private void ButtonLoad_MouseClick(object sender, MouseEventArgs e)
         {
-            // ADD CODE
+            LoadData();
         }
 
         private void ButtonBinarySearch_MouseClick(object sender, MouseEventArgs e)
@@ -82,8 +80,7 @@ namespace WikiApp
         
         private void ButtonSort_MouseClick(object sender, MouseEventArgs e)
         {
-            BubbleSort();
-            // ADD CODE
+            SortTable();
         }
         #endregion
 
@@ -92,9 +89,34 @@ namespace WikiApp
         /// <summary>
         /// Bubble sort function
         /// </summary>
-        private void BubbleSort()
+        private void BubbleSort(int indx)
         {
-            // ADD CODE
+            string temp;
+            for (int z = 0; z < Col; z++)
+            {
+                temp = WikiArray[indx, z];
+                WikiArray[indx, z] = WikiArray[indx + 1, z];
+                WikiArray[indx + 1, z] = temp;
+            }
+        }
+
+        /// <summary>
+        /// Sort table
+        /// </summary>
+        private void SortTable()
+        {
+            for (int x = 0; x < Row; x++)
+            {
+                for (int i = 0; i < Row - 1; i++)
+                {
+                    if (string.Compare(WikiArray[i, 0], WikiArray[i + 1, 0]) == 1)
+                    {
+                        BubbleSort(i);
+                    }
+                }
+            }
+            DisplayData();
+            sorted = true;
         }
 
         // 9.7 Write the code for a Binary Search for the Name in the 2D array and display the information in the other textboxes when found, add suitable feedback if the search in not successful and clear the search textbox (do not use any built-in array methods)
@@ -102,37 +124,124 @@ namespace WikiApp
         /// Binary search function that would return a index
         /// </summary>
         /// <returns></returns>
-        private int BinarySearch()
+        private int BinarySearch(int searchValue)
         {
-            // ADD CODE
+     
+            int lastIndex = Row - 1;
+            int firstIndex = 0;
+            int searchIndex;
+
+            while (firstIndex <= lastIndex)
+            {
+                searchIndex = (firstIndex + lastIndex) / 2;
+
+                // if searched value equals to the search index, then its a matched and its index will return
+                if (WikiArray[searchIndex, 0] == searchValue.ToString())
+                {
+                    return searchIndex;
+                }
+                // if the searched value bigger than then current search index, then it will continue its search on the right hand of the array 
+                else if (WikiArray[searchIndex, 0] < searchValue.ToInt())
+                {
+                    firstIndex = searchIndex + 1;
+                }
+                // if the searched value is smaller than the current search index, then it will continue its search on the left hand of the array
+                else
+                {
+                    lastIndex = searchIndex - 1;
+                }
+
+            }
             return -1;
+
         }
 
         // 9.8 Create a display method that will show the following information in a ListView: Name and Category
         /// <summary>
         /// Display function
         /// </summary>
-        private void Display()
+        private void DisplayData()
         {
-            // ADD CODE
+            ListViewDataStructure.Items.Clear();
+            for (int x = 0; x < Row; x++)
+            {
+                ListViewItem item = new ListViewItem(WikiArray[x, 0]);
+                item.SubItems.Add(WikiArray[x, 1]);
+                item.SubItems.Add(WikiArray[x, 2]);
+                item.SubItems.Add(WikiArray[x, 3]);
+                // item.SubItems.Add(WikiArray[x, 4]);
+                ListViewDataStructure.Items.Add(item);
+            }
         }
 
         // 9.9 Create a method so the user can select a definition (Name) from the ListView and all the information is displayed in the appropriate Textboxes
         /// <summary>
         /// Select function
         /// </summary>
-        private void Select()
+        private void SelectItem(int index)
         {
-            // ADD CODE
+            //ListViewDataStructure.SelectedItems();
+                //(index, true);
+            TextboxSearch.Clear();
+            TextboxSearch.Text = ListViewDataStructure.SelectedItems.ToString();
         }
 
         /// <summary>
         /// Update list box function
         /// </summary>
-        private void UpdateList()
+        private void LoadData()
+        {
+            Random random = new Random();
+            for (int x = 0; x < Row; x++)
+            {
+                for (int y = 0; y < Col; y++)
+                {
+                    WikiArray[x, y] = random.Next(10, 99).ToString();
+                }
+            }
+            DisplayData();
+        }
+
+        /// <summary>
+        /// Save file function
+        /// </summary>
+        /// 
+        private void SaveFile()
         {
             // ADD CODE
         }
+
+        /// <summary>
+        /// Clear method
+        /// </summary>
+        private void ClearData()
+        {
+            // ADD CODE
+        }
+
+        /// <summary>
+        /// Update status strip
+        /// </summary>
+        private void UpdateStatusStrip(string message, string status, MessageBoxIcon icon)
+        {
+            MessageBox.Show(message, status, MessageBoxButtons.OK, icon);
+            StatusLabel.Text = message;
+            TextboxSearch.Focus();
+        }
+
+        private void UpdateProgressBar(int value)
+        {
+            StatusProgressBar.Value = value;
+        }
+
+        private bool DeleteConfirmationMessage(object selectedItem)
+        {
+            var message = $"Are you sure you want to delete {selectedItem}?";
+            var caption = $"Delete {selectedItem}?";
+            return MessageBox.Show(message, caption, MessageBoxButtons.YesNo, MessageBoxIcon.Information)
+                    == DialogResult.Yes;
+        }
+
         #endregion
     }
 }
